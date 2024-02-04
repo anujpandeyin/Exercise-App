c90e2ff9318e35248521c245e2cf531f6d4877b7
6theme <gtheme.com@gmail.com>
2024-02-05 03:32:49 +0530

    done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
       <Stack gap='40px' alignItems='center' px='40px' pt='24px'>
         <img src={Logo} alt='logo' width='200px' height='40px'/>
         <Typography variant='h5' pb='40px' mt='20px'>
-Build WIth LOve BY Anuj Pandey        </Typography>
+         Build WIth Love BY Anuj Pandey        </Typography>
       </Stack>
     </Box>
   )
